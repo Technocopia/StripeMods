319ec97ab54d02e7f361f1b0757d76e45aa7c5b5
Kevin Harrington <mad.hephaestus@gmail.com>
2021-06-22 16:55:28 -0400

    adding google sheets interface

--- StripeMods/src/main/java/org/technocopia/Main.java
+++ StripeMods/src/main/java/org/technocopia/Main.java
@@ -15,23 +15,100 @@ import com.stripe.model.SubscriptionCollection;
 import com.stripe.model.SubscriptionItem;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 	public static boolean live =false;
 	public static CardReaderCommand command;
 	
-	public static void main(String[] args) throws StripeException {
+    private static final String APPLICATION_NAME = "Technocopia Sign-up Keiosk";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = Keys.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+    
+    
+	public static void main(String[] args) throws Exception {
 
 		Stripe.apiKey = Keys.Secret;
 		command=new CardReaderCommand();
-		command.setGotCard(new IOnCardRead() {
-			
-			@Override
-			public void event(long newNumber) {
-				System.out.println("Got card! "+newNumber);
-			}
-		});
+		command.setGotCard(newNumber -> System.out.println("Got card! "+newNumber));
 		
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        final String spreadsheetId = "1uw4HVfUe_84FCFyCGBQeNqwv7I2fgep0OSJr1r80ZYM";
+        final String range = "Form Responses 1!B1:T";
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            System.out.println("Name, Major");
+            for (int i=0;i<values.size();i++) {
+            	List row =values.get(i);
+                // Print columns A and E, which correspond to indices 0 and 4.
+            	try {
+            		System.out.println("Row # "+i+" value= "+ row.get(0)+" "+row.get(2)+" "+row.get(7));
+            	}catch(IndexOutOfBoundsException ex) {
+            		System.out.println("No data on line "+ i);
+            	}
+            }
+        }
+   
+		System.exit(0);
 //		Map<String, Object> params = new HashMap<>();
 //		// params.put("email", "monroe.lauren@gmail.com");
 //		CustomerCollection customers = Customer.list(params);
