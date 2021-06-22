a5301a6c6c998f169fa7995bb9ef5743b1a18733
Kevin Harrington <mad.hephaestus@gmail.com>
2021-06-22 15:29:39 -0400

    try catch

--- StripeMods/src/main/java/org/technocopia/CardReaderCommand.java
+++ StripeMods/src/main/java/org/technocopia/CardReaderCommand.java
@@ -5,19 +5,25 @@ import edu.wpi.SimplePacketComs.phy.HIDSimplePacketComs;
 
 public class CardReaderCommand extends HIDSimplePacketComs {
 	FloatPacketType readCard = new FloatPacketType(44, 64);
-	double[] card=new double[1];
-	private IOnCardRead gotCard =null;
-	public CardReaderCommand(){
-		super(0x16c0,  0x486);
+	double[] card = new double[1];
+	private IOnCardRead gotCard = null;
+
+	public CardReaderCommand() {
+		super(0x16c0, 0x486);
 		connect();
 		readCard.pollingMode();
 		addPollingPacket(readCard);
-		
+
 		addEvent(readCard.idOfCommand, () -> {
-			readFloats(readCard.idOfCommand, card);
-			long value = Math.round(card[0]);
-			if(value>0 && gotCard!=null) {
-				gotCard.event(value);
+			try {
+				readFloats(readCard.idOfCommand, card);
+				long value = Math.round(card[0]);
+				if (value > 0 && gotCard != null) {
+					gotCard.event(value);
+				}
+			} catch (Throwable t) {
+				t.printStackTrace();
+
 			}
 		});
 	}
